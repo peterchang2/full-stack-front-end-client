2deb07afb463a5409a7f13c0d974f33c45bf4d75
peterchang2 <peterchang664@gmail.com>
2018-12-07 10:30:48 -0500

    upload function started

---
+++ assets/scripts/upload/api.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const store = require('../store.js')
+const config = require('../config.js')
+
+const upload = function (inputData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/images/',
+    contentType: 'application/json',
+    data: JSON.stringify(inputData)
+  })
+}
+
+const signIn = function (inputData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in/',
+    contentType: 'application/json',
+    data: JSON.stringify(inputData)
+  })
+}
+
+const changePassword = function (inputData) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password/',
+    contentType: 'application/json',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
+    data: JSON.stringify(inputData)
+  })
+}
+
+const signOut = function (inputData) {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/sign-out/',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
+module.exports = {
+  upload
+}
