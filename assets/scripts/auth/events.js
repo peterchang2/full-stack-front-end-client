4b7455b3f127eefc1bb23bbfdac48e47158769ac
peterchang2 <peterchang664@gmail.com>
2018-12-10 21:33:05 -0500

    Added password does not match message, added minimum password length message

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,10 +6,14 @@ const ui = require('./ui.js')
 const onSignUp = function (event) {
   event.preventDefault()
   const userData = getFormFields(event.target)
-  $(event.target).trigger('reset')
-  api.signUp(userData)
-    .then(ui.signUpSuccess)
-    .catch(ui.failure)
+  if ($('.pass').val() !== $('.confirmpass').val()) {
+    ui.passDoesntMatch()
+  } else {
+    $(event.target).trigger('reset')
+    api.signUp(userData)
+      .then(ui.signUpSuccess)
+      .catch(ui.failure)
+  }
 }
 
 const onSignIn = function (event) {
@@ -19,7 +23,7 @@ const onSignIn = function (event) {
   $(event.target).trigger('reset')
   api.signIn(userData)
     .then(ui.signInSuccess)
-    .catch(ui.failure)
+    .catch(ui.signInFailure)
 }
 
 const onChangePassword = function () {
